0bd2a957bb758569cca6311cb1a1fe1fb2ae8683
Koichi Yokota <sushifarm2012@gmail.com>
2022-02-24 21:49:47 +0900

    Add error descriptions to `FileError`

--- Sources/Puppy/FileError.swift
+++ Sources/Puppy/FileError.swift
@@ -1,20 +1,27 @@
 import Foundation
 
-public enum FileError: Error, Equatable {
+public enum FileError: Error, Equatable, LocalizedError {
     case isNotFile(url: URL)
+    case invalidPermission(at: URL, filePermission: String)
     case creatingDirectoryFailed(at: URL)
     case creatingFileFailed(at: URL)
-    case writingFailed(at: URL)
-    case invalidPermission(at: URL, filePermission: String)
-}
-
-public enum FileDeletingError: Error, Equatable, LocalizedError {
-    case failed(at: URL)
+    case openingForWritingFailed(at: URL)
+    case deletingFailed(at: URL)
 
     public var errorDescription: String? {
         switch self {
-        case .failed(at: let url):
-            return "failed to delete the file: \(url)"
+        case .isNotFile(url: let url):
+            return "\(url) is not a file"
+        case .invalidPermission(at: let url, filePermission: let filePermission):
+            return "invalid file permission. file: \(url), permission: \(filePermission)"
+        case .creatingDirectoryFailed(at: let url):
+            return "failed to create a directory: \(url)"
+        case .creatingFileFailed(at: let url):
+            return "failed to create a file: \(url)"
+        case .openingForWritingFailed(at: let url):
+            return "failed to open a file for writing: \(url)"
+        case .deletingFailed(at: let url):
+            return "failed to delete a file: \(url)"
         }
     }
 }
