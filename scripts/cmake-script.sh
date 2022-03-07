056dd5367d1b60083f54f0678eaf44270977060e
Koichi Yokota <sushifarm2012@gmail.com>
2022-03-07 21:33:13 +0900

    Update scripts for CI

--- scripts/cmake-script.sh
+++ scripts/cmake-script.sh
@@ -2,38 +2,74 @@
 
 set -eu
 
-# Intall packates.
-declare -a packages=()
+DISTRIBUTION=${DISTRIBUTION:-'focal'}
+CMAKE_VERSION=${CMAKE_VERSION:-'3.22.2'}
+NINJA_VERSION=${NINJA_VERSION:-'1.10.2'}
 
-case $(uname) in
-  Darwin)
-    if ! type cmake > /dev/null 2>&1; then packages+=('cmake'); fi
-    if ! type ninja > /dev/null 2>&1; then packages+=('ninja'); fi
-    if [ ${#packages[@]} -ne 0 ]; then
-      brew update && brew install ${packages[@]}
-    fi
-    ;;
-  Linux)
-    if ! type cmake > /dev/null 2>&1; then packages+=('cmake'); fi
-    if ! type ninja > /dev/null 2>&1; then packages+=('ninja-build'); fi
-    if [ ${#packages[@]} -ne 0 ]; then
-      if [ -e '/.dockerenv' ]; then
-        apt-get -q update && apt-get -q install -y ${packages[@]}
-      else
-        sudo apt-get -q update && sudo apt-get -q install -y ${packages[@]}
+# macOS
+if [ $(uname) = 'Darwin' ]; then
+  declare -a packages=()
+  if ! type cmake > /dev/null 2>&1; then packages+=('cmake'); fi
+  if ! type ninja > /dev/null 2>&1; then packages+=('ninja'); fi
+  if [ ${#packages[@]} -ne 0 ]; then
+    brew update && brew install ${packages[@]}
+  fi
+fi
+
+# Linux
+if [ $(uname) = 'Linux' ] && [ ! -e '/.dockerenv' ]; then
+  declare -a packages=()
+  if ! type cmake > /dev/null 2>&1; then packages+=('cmake'); fi
+  if ! type ninja > /dev/null 2>&1; then packages+=('ninja'); fi
+  if [ ${#packages[@]} -ne 0 ]; then
+    sudo apt-get -qq update && sudo apt-get -qq install -y ${packages[@]}
+  fi
+fi
+
+# Linux Container
+if [ $(uname) = 'Linux' ] && [ -e '/.dockerenv' ]; then
+  declare -a packages=()
+  if ! type curl > /dev/null 2>&1; then packages+=('curl'); fi
+  if ! type unzip > /dev/null 2>&1; then packages+=('unzip'); fi
+
+  case ${DISTRIBUTION} in
+    focal|bionic)
+      if [ ${#packages[@]} -ne 0 ]; then
+        apt-get -qq update && apt-get -qq install -y ${packages[@]}
       fi
-    fi
-    ;;
-  *) # Assume OS is Windows.
-    ;;
-esac
+      ;;
+    amazonlinux2|centos7)
+      if [ ${#packages[@]} -ne 0 ]; then
+        yum -q -y update && yum -q -y install ${packages[@]}
+      fi
+      ;;
+    *)
+      echo "unexpected distribution: ${DISTRIBUTION}"
+      exit 1
+      ;;
+  esac
 
+  # Install CMake.
+  echo "Download CMake ${CMAKE_VERSION} ..."
+  curl -sL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" -o "/tmp/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz"
+  tar xfz "/tmp/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" -C /opt
+  export PATH="/opt/cmake-${CMAKE_VERSION}-linux-x86_64/bin:${PATH}"
+  if [ -v GITHUB_ACTIONS ]; then
+    echo "/opt/cmake-${CMAKE_VERSION}-linux-x86_64/bin:${PATH}" >> $GITHUB_PATH
+  fi
+  # Install Ninja.
+  echo "Download Ninja ${NINJA_VERSION} ..."
+  curl -sL "https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-linux.zip" -o "/tmp/ninja-linux.zip"
+  unzip /tmp/ninja-linux.zip -d /usr/bin
+  chmod u+x /usr/bin/ninja
+fi
+
+# Show versions.
 cmake --version
 ninja --version
 
 # Build with CMake and Ninja.
 rm -rf build
-
 case $(uname) in
   Darwin|Linux)
     cmake -B ./build -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S .
