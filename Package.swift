ebe2cdb3549aa6b982fb8161732060345f8e61af
Neil Tiffin <neil@fairwindsoft.com>
2026-07-30 10:36:40 -0500

    Update to macOS v12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Puppy",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
+        .macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
     ],
     products: [
         .library(name: "Puppy", targets: ["Puppy"]),
