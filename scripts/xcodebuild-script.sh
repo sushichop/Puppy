470e59dc43665a4dfdfc40f60b50f008f41cb5a5
Koichi Yokota <sushifarm2012@gmail.com>
2026-07-31 01:12:14 +0100

    Update Xcode to `26.6` (#111)
    Update Xcode to `26.6`

--- scripts/xcodebuild-script.sh
+++ scripts/xcodebuild-script.sh
@@ -8,12 +8,12 @@ SCRIPT_TYPE=${SCRIPT_TYPE:-build}
 xcodebuild -showsdks
 xcrun simctl list devices
 
-dests[0]="platform=macOS"
-dests[1]="platform=macOS,variant=Mac Catalyst"
-dests[2]="platform=iOS Simulator,OS=latest,name=iPhone 16 Pro"
-dests[3]="platform=tvOS Simulator,OS=latest,name=Apple TV 4K (3rd generation)"
-dests[4]="platform=watchOS Simulator,OS=latest,name=Apple Watch Series 10 (46mm)"
-dests[5]="platform=visionOS Simulator,OS=latest,name=Apple Vision Pro"
+dests[0]="platform=macOS,arch=arm64"
+dests[1]="platform=macOS,arch=arm64,variant=Mac Catalyst"
+dests[2]="platform=iOS Simulator,arch=arm64,OS=latest,name=iPhone 17 Pro"
+dests[3]="platform=tvOS Simulator,arch=arm64,OS=latest,name=Apple TV 4K (3rd generation)"
+dests[4]="platform=watchOS Simulator,arch=arm64,OS=latest,name=Apple Watch Series 11 (46mm)"
+dests[5]="platform=visionOS Simulator,arch=arm64,OS=latest,name=Apple Vision Pro"
 
 for dest in "${dests[@]}"
 do
